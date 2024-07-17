5fbfc443debc436acfa926aae95d68b38e797ced
unknown <ouattara.abraham2019@gmail.com>
2024-07-17 06:46:44 +0100

    pufff

--- src/components/Boules.jsx
+++ src/components/Boules.jsx
@@ -1,16 +1,16 @@
-import { ScrollRotate } from "react-scroll-rotate";
+// import { ScrollRotate } from "react-scroll-rotate";
 import propTypes from 'prop-types'
 
 const Boules = ({bgColor}) => {
   return (
     <div className={`${bgColor} bgCol flex items-center justify-center`}>
-      <ScrollRotate>
+      {/* <ScrollRotate> */}
         <img
           className="w-[50px] lg:w-[70px]"
           src="../src/assets/icon/boulle.svg"
           alt=""
         />
-      </ScrollRotate>
+      {/* </ScrollRotate> */}
     </div>
   );
 };
